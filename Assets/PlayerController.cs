660a2c0e645d0107df87b397563cc9b448e63f96
MajaGren <maja0gren@gmail.com>
2023-04-15 17:46:46 +0200

    Grafika

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    private float moveInput;
+
+    void Start()
+    {
+    rb = GetComponent<Rigidbody2D>();
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+}
