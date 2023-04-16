791ce239b1b6a6fe44bbcbd4669d16491f278b57
MajaGren <maja0gren@gmail.com>
2023-04-16 07:14:37 +0200

    Triggers

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,7 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed;
+    public float jumpForce;
     private float moveInput;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+     public int points = 0;
 
     void Start()
     {
@@ -19,5 +27,27 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+     private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Trash"))
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                points += 1;
+               
+                Destroy(other.gameObject);
+            }
+        }
     }
 }
